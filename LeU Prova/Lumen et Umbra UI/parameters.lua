075791e1aedad95e126677d80341ca1938c83000
Ken Ciro <kenciro78@hotmail.com>
2024-05-02 23:17:22 +0200

    formatting code

--- LeU Prova/Lumen et Umbra UI/parameters.lua
+++ LeU Prova/Lumen et Umbra UI/parameters.lua
@@ -1,3 +1,3 @@
-return {{[ [[page]] ] = [[a]],[ [[fontSize]] ] = {2},},
-{[ [[equipment]] ] = 8,[ [[overland]] ] = 8,[ [[help]] ] = 8,[ [[keybind]] ] = 6,[ [[room]] ] = 7,[ [[inventory]] ] = 8,[ [[privateChat]] ] = 8,[ [[publicChat]] ] = 8,},
+return {{[ [[fontSize]] ] = {2},[ [[page]] ] = [[a]],[ [[enablePugno]] ] = true,},
+{[ [[equipment]] ] = 8,[ [[overland]] ] = 8,[ [[help]] ] = 12,[ [[keybind]] ] = 6,[ [[room]] ] = 7,[ [[inventory]] ] = 8,[ [[privateChat]] ] = 8,[ [[publicChat]] ] = 8,},
 }
