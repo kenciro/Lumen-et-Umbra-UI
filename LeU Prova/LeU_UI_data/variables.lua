7ff4f437e6ca4c9352ce9713ec3c9b97663736a9
Maurizio Luciano <maurizio.luciano@sirius-es.it>
2024-10-01 17:55:03 +0200

    Map update Widget room minor fix Minor fix

--- LeU Prova/LeU_UI_data/variables.lua
+++ LeU Prova/LeU_UI_data/variables.lua
@@ -1,2 +1,2 @@
-return {{[ [[bag]] ] = [[borsa-conservante]],[ [[bag2]] ] = [[sacca]],[ [[target]] ] = [[]],[ [[stat]] ] = [[des]],[ [[weapon]] ] = [[bacchetta]],},
+return {{[ [[bag]] ] = [[borsa-conservante]],[ [[bag2]] ] = [[sacca]],[ [[target]] ] = [[]],[ [[stat]] ] = [[int]],[ [[weapon]] ] = [[bacchetta]],},
 }
