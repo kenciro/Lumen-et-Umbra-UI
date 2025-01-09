66f014bf0465cd54140908b5dc48a772c96fe687
Ciro <kenciro78@hotmail.com>
2025-01-09 11:01:15 +0100

    Delete LeU Live/telegram_options.lua

--- LeU Live/telegram_options.lua
+++
@@ -1,4 +0,0 @@
-return {{[ [[Enabled]] ] = true,[ [[PendingAuthorization]] ] = {2},[ [[Token]] ] = [[5475936010:AAGeaRzdJYqzqWpm6xU8dYGzwFykkZUeTrs]],[ [[Interval]] ] = 10,[ [[Authorized]] ] = {3},},
-{},
-{[ [[KenCiro78]] ] = 543284657,},
-}
